de0f6b5818646446f4095a696c466ab5b6ae035b
gaoyx <melo@gaoyuexiang.cn>
2016-07-02 16:55:54 +0800

    add Item modle

---
+++ backEnd/src/main/java/cn/gaoyuexiang/model/Item.java
@@ -0,0 +1,71 @@
+package cn.gaoyuexiang.model;
+
+/**
+ * Created by melo on 16-7-2.
+ */
+public class Item {
+
+	private String barcode;
+	private String name;
+	private String unit;
+	private String category;
+	private String subCategory;
+	private double price;
+
+	public Item(String barcode, String name, String unit, String category, String subCategory, double price) {
+		this.barcode = barcode;
+		this.name = name;
+		this.unit = unit;
+		this.category = category;
+		this.subCategory = subCategory;
+		this.price = price;
+	}
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getSubCategory() {
+		return subCategory;
+	}
+
+	public void setSubCategory(String subCategory) {
+		this.subCategory = subCategory;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
