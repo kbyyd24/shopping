c7901e82073f44eefdde364c0b531f914380f916
Summer-Dong <D__yanan@163.com>
2016-07-07 15:21:35 +0800

    try to solve http conflict

--- src/main/java/cn/gaoyuexiang/controller/ItemController.java
+++ src/main/java/cn/gaoyuexiang/controller/ItemController.java
@@ -2,12 +2,15 @@ package cn.gaoyuexiang.controller;
 
 import cn.gaoyuexiang.model.Item;
 import cn.gaoyuexiang.service.ItemService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import org.apache.struts2.ServletActionContext;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -15,19 +18,24 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * Created by melo on 16-7-2.
  * demo controller
  */
+
 @RestController
 @RequestMapping("/item")
 public class ItemController {
 	private ItemService itemService;
-
+	private HttpServletResponse rps=ServletActionContext.getResponse();
+	@ModelAttribute
+	public void  SetVaryResponseHeader(HttpServletResponse response){
+		response.addHeader("Access-Control-Allow-Origin","*");
+	}
 	@Autowired
 	public ItemController(ItemService itemService) {
 		this.itemService = itemService;
 	}
-
 	@ResponseBody
 	@RequestMapping(method = GET)
 	public List<Item> getItems() {
+		SetVaryResponseHeader(rps);
 		return itemService.getItems();
 	}
 }
