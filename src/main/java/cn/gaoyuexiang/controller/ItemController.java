5066691c2ff0190a819b49df265573a46b19ac83
gaoyx <melo@gaoyuexiang.cn>
2016-07-08 21:04:28 +0800

    [N/A] remove ResponseBody annotation

--- src/main/java/cn/gaoyuexiang/controller/ItemController.java
+++ src/main/java/cn/gaoyuexiang/controller/ItemController.java
@@ -2,15 +2,12 @@ package cn.gaoyuexiang.controller;
 
 import cn.gaoyuexiang.model.Item;
 import cn.gaoyuexiang.service.ItemService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-//import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
-//import org.apache.struts2.ServletActionContext;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -24,11 +21,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 public class ItemController {
 	private ItemService itemService;
+
 	@Autowired
 	public ItemController(ItemService itemService) {
 		this.itemService = itemService;
 	}
-	@ResponseBody
+
 	@CrossOrigin
 	@RequestMapping(method = GET)
 	public List<Item> getItems() {
