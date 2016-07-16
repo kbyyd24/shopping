04a026e4a884f5af2d452a1ca5e0b90a72b881fa
gaoyx <melo@gaoyuexiang.cn>
2016-07-16 15:32:51 +0800

    [N/A] 抽离计算总计的代码

--- src/main/java/cn/gaoyuexiang/service/sale/SaleStrategy.java
+++ src/main/java/cn/gaoyuexiang/service/sale/SaleStrategy.java
@@ -1,6 +1,9 @@
 package cn.gaoyuexiang.service.sale;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> cfedffe... [N/A] 抽离计算总计的代码
 import cn.gaoyuexiang.model.PaymentItem;
 import cn.gaoyuexiang.model.PaymentResult;
 
@@ -8,7 +11,10 @@ import java.util.List;
 
 public interface SaleStrategy {
 	PaymentResult calculate(List<PaymentItem> items);
+<<<<<<< HEAD
 =======
 public interface SaleService {
 >>>>>>> c777cf5... add SaleStrtegy
+=======
+>>>>>>> cfedffe... [N/A] 抽离计算总计的代码
 }
