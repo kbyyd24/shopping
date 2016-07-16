c51012256d6b06eed8efceb54f582280b9451828
gaoyx <melo@gaoyuexiang.cn>
2016-07-16 16:05:00 +0800

    [N/A] 修复merge错误

--- src/main/java/cn/gaoyuexiang/service/sale/SaleStrategy.java
+++ src/main/java/cn/gaoyuexiang/service/sale/SaleStrategy.java
@@ -1,9 +1,5 @@
 package cn.gaoyuexiang.service.sale;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> cfedffe... [N/A] 抽离计算总计的代码
 import cn.gaoyuexiang.model.PaymentItem;
 import cn.gaoyuexiang.model.PaymentResult;
 
@@ -11,10 +7,4 @@ import java.util.List;
 
 public interface SaleStrategy {
 	PaymentResult calculate(List<PaymentItem> items);
-<<<<<<< HEAD
-=======
-public interface SaleService {
->>>>>>> c777cf5... add SaleStrtegy
-=======
->>>>>>> cfedffe... [N/A] 抽离计算总计的代码
 }
