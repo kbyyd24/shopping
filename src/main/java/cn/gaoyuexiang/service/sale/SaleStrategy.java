f0c6ca478960c59d67d688d34ea70bb9efa13a53
gaoyx <melo@gaoyuexiang.cn>
2016-07-16 15:32:12 +0800

    [N/A] add SaleStrtegy

--- src/main/java/cn/gaoyuexiang/service/sale/SaleStrategy.java
+++ src/main/java/cn/gaoyuexiang/service/sale/SaleStrategy.java
@@ -1,5 +1,6 @@
 package cn.gaoyuexiang.service.sale;
 
+<<<<<<< HEAD
 import cn.gaoyuexiang.model.PaymentItem;
 import cn.gaoyuexiang.model.PaymentResult;
 
@@ -7,4 +8,7 @@ import java.util.List;
 
 public interface SaleStrategy {
 	PaymentResult calculate(List<PaymentItem> items);
+=======
+public interface SaleService {
+>>>>>>> c777cf5... add SaleStrtegy
 }
